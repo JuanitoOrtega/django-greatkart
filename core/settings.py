bfd1ae67d7b3ee3a4b80086b843d798fd2c3180a
Juanito Ortega <ortegaj83@gmail.com>
2022-09-08 22:53:11 -0400

    store app and product model

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # Custom apps
     'category',
     'users',
+    'store',
 ]
 
 MIDDLEWARE = [
---
+++ store/models.py
@@ -0,0 +1,23 @@
+from enum import unique
+from django.db import models
+from category.models import Category
+
+# Create your models here.
+class Product(models.Model):
+  product_name = models.CharField(max_length=255, unique=True, verbose_name='Producto')
+  slug = models.SlugField(max_length=255, unique=True, verbose_name='Slug')
+  description = models.TextField(max_length=500, blank=True, verbose_name='Descripción')
+  price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Precio')
+  images = models.ImageField(upload_to='photos/products', verbose_name='Galería')
+  stock = models.IntegerField(blank=True, verbose_name='Cantidad en mano')
+  is_available = models.BooleanField(default=True, verbose_name='Disponible')
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Categoría')
+  created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de registro')
+  updated_at = models.DateTimeField(auto_now=True, verbose_name='Actualizado')
+
+  class Meta:
+    verbose_name = 'Producto'
+    verbose_name_plural = 'Productos'
+
+  def __str__(self):
+    return self.product_name
