07288260133a798face1f52528e734da52de4e7a
Juanito Ortega <ortegaj83@gmail.com>
2022-09-10 11:11:21 -0400

    displaying categories on navbar

--- category/models.py
+++ category/models.py
@@ -1,6 +1,5 @@
-from distutils.command.upload import upload
-from unicodedata import category
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -12,6 +11,9 @@ class Category(models.Model):
   class Meta:
     verbose_name = 'Categoría'
     verbose_name_plural = 'Categorías'
+
+  def get_url(self):
+    return reverse('products_by_category', args=[self.slug])
 
   def __str__(self):
     return self.category_name
