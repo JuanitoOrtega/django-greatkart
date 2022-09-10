3f453679db740df3eca124344e199bab5d3e973a
Juanito Ortega <ortegaj83@gmail.com>
2022-09-10 02:14:21 -0400

    deploying

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,13 +21,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j=o%2l&rx7=f$2&$ib&9p%8@&pry6snu0#3mh*)um1g1a4f#tr'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['greatkart-full-env.eba-fhmngqah.us-west-2.elasticbeanstalk.com']
-
 
 # Application definition
 
@@ -134,6 +134,13 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'core/static'
 ]
+
+# Configuración para enviar correos
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
 # Media files configuration
 MEDIA_ROOT = BASE_DIR / 'media'
