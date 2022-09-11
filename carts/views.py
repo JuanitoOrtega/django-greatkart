c5540cadca5d8d8b28252cf25b4ac569b59a7271
Juanito Ortega <ortegaj83@gmail.com>
2022-09-11 10:22:33 -0400

    search function

--- carts/views.py
+++ carts/views.py
@@ -55,9 +55,9 @@ def remove_cart_item(request, product_id):
   return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_items=None):
-  tax = 0
-  grand_total = 0
   try:
+    tax = 0
+    grand_total = 0
     cart = Cart.objects.get(cart_id=_cart_id(request))
     cart_items = CartItem.objects.filter(cart=cart, is_active=True)
     for cart_item in cart_items:
--- store/views.py
+++ store/views.py
@@ -4,7 +4,8 @@ from .models import Product
 from category.models import Category
 from carts.views import _cart_id
 # from django.http import HttpResponse
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -14,13 +15,13 @@ def store(request, category_slug=None):
   if category_slug != None:
     categories = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=categories, is_available=True)
-    paginator = Paginator(products, 6)
+    paginator = Paginator(products, 3)
     page = request.GET.get('page')
     paged_products = paginator.get_page(page)
     product_count = products.count()
   else:
-    products = Product.objects.all().filter(is_available=True)
-    paginator = Paginator(products, 6)
+    products = Product.objects.all().filter(is_available=True).order_by('id')
+    paginator = Paginator(products, 3)
     page = request.GET.get('page')
     paged_products = paginator.get_page(page)
     product_count = products.count()
@@ -47,3 +48,26 @@ def product_detail(request, category_slug, product_slug):
   }
 
   return render(request, 'store/product_detail.html', context)
+
+def search(request):
+  products = None
+  paged_products = None
+  product_count = None
+  if 'keyword' in request.GET:
+    keyword = request.GET['keyword']
+    if keyword:
+      products = Product.objects.order_by('-created_at').filter(Q(product_name__icontains=keyword) | Q(description__icontains=keyword))
+      if 'keyword' in request.GET and request.GET['keyword']:
+        page = request.GET.get('page')
+        keyword = request.GET['keyword']
+        paginator = Paginator(products, 3)
+      paged_products = paginator.get_page(page)
+      product_count = products.count()
+  
+  context = {
+    'products': paged_products,
+    'product_count': product_count,
+    'keyword': keyword,
+  }
+
+  return render(request, 'store/store.html', context)
