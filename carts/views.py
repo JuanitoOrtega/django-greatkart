3ce0d191844b680e9cd4509fd8530ad650b44f3d
Juanito Ortega <ortegaj83@gmail.com>
2022-09-10 20:24:37 -0400

    calculate tax & grand total

--- carts/views.py
+++ carts/views.py
@@ -37,12 +37,17 @@ def add_cart(request, product_id):
   return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_items=None):
+  tax = 0
+  grand_total = 0
   try:
     cart = Cart.objects.get(cart_id=_cart_id(request))
     cart_items = CartItem.objects.filter(cart=cart, is_active=True)
     for cart_item in cart_items:
       total += (cart_item.product.price * cart_item.quantity)
       quantity += cart_item.quantity
+    # IVA
+    tax = round((2 * total) / 100, 2) # redondea a 2 decimales
+    grand_total = round(total + tax, 2)
   except ObjectDoesNotExist:
     pass # just ignore
 
@@ -50,6 +55,8 @@ def cart(request, total=0, quantity=0, cart_items=None):
     'total': total,
     'quantity': quantity,
     'cart_items': cart_items,
+    'tax': tax,
+    'grand_total': grand_total,
   }
 
   return render(request, 'store/cart.html', context)
